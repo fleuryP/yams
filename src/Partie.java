e0e6f6e679ef92a7d3889d61bc69d3ff41c45429
fleuryP <71771254+fleuryP@users.noreply.github.com>
2021-12-07 09:32:53 +0100

    Update partie.java
    Implémentation de "Joueur" et "TourCourant" pour tester le tableau des "Score"

--- src/Partie.java
+++ src/Partie.java
@@ -1,9 +1,28 @@
 
 public class Partie {
-	int nbJoueurs, nbTours;
+	int nbJoueurs=6;
+	int nbTours;
 	TourCourant tc;
 	Strategie strat;
 	
+	public static void main (String [] args) {
+		TourCourant tC = new TourCourant();
+		Score score = new Score(3);
+		
+		Joueur j=new Joueur("Pierre");
+		score.nomJoueur(j.getNom());
+		
+		Joueur j2=new Joueur("Célia");
+		score.nomJoueur(j2.getNom());
+		
+		Joueur j3=new Joueur("Marine");
+		score.nomJoueur(j3.getNom());
+		
+		//score.afficherScore();
+		
+		System.out.println(tC.toString());
+		
+		
+	}
 	
-
 }
