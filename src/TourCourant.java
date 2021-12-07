8467fa6189038fe25c10fa3edd00f1d41f5f0bac
Jordan-Celia1 <71645007+Jordan-Celia1@users.noreply.github.com>
2021-12-07 09:52:07 +0100

    Update TourCourant.java
    modification relance et reponse, maintenant fonctionne

--- src/TourCourant.java
+++ src/TourCourant.java
@@ -32,21 +32,18 @@ public class TourCourant {
 		
 	}
 	public void relancer(int[] tab) { 
-		System.out.println("aaaa"+Arrays.toString(tab)+"la longueur: "+tab.length);
 		for (int i=0;i<tab.length;i++) { 
-			if(!(tab[i]-1==0)){ // si tab[i]-1 n'est pas égal à -1, soit si il a pas été modifier 
-				des[tab[i]-1].setValeur(des[tab[i]-1].lancer()); // on relance le dé a la position donnée
-				// ne fonctionne pas correctement a voir pourquoi 
-			}
+			if(!(tab[i]==0)){ 
+				des[tab[i]-1].setValeur(des[tab[i]-1].lancer()); // on relance le dé a la position donnée			
+				}
 		}
 	}
-	// mettre a -1
+	
 	
 	public int[] reponse(String s) {
 		int [] tab = {0,0,0,0,0}; // tableau de 5 valeurs car 5 des
 		for(int i=0; i<s.length(); i++) { // tant que l'on a pas regarder tout les caractères de la chaine
 			tab[i]=Character.getNumericValue(s.charAt(i)); // on stock chaque caractère dans un tableau
-			System.out.println(Arrays.toString(tab));
 		}
 		return tab;
 	}
