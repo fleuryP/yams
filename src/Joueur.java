dada77a5cd7e63c9beb6cc50ab29708e397d0f3c
fleuryP <71771254+fleuryP@users.noreply.github.com>
2021-12-08 17:55:40 +0100

    Update Joueur.java

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,12 +1,17 @@
 
 public class Joueur {
 	
-	private String nom;
+	String nom;
 	private int score;
 	
-	public Joueur(String nom) {
+	//permet de savoir à quelle place il est dans le tableau des score 
+	// car on a besoin de savoir sa colonne
+	private int position;
+	
+	public Joueur(String nom, int position) {
 		this.nom=nom;
 		this.score=0;
+		this.position=position;
 	}
 	
 	public String getNom() {
@@ -15,6 +20,10 @@ public class Joueur {
 	
 	public int getScore() {
 		return score;
+	}
+	
+	public int getPosition() {
+		return position;
 	}
 	
 	public void ajouterPoints(int nbPoints) {
