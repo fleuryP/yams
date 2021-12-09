33c79716ed7a092e4f3195257503a766d38c7f17
fleuryP <71771254+fleuryP@users.noreply.github.com>
2021-12-09 12:08:42 +0100

    update Partie.java

--- src/Partie.java
+++ src/Partie.java
@@ -1,3 +1,4 @@
+package version1;
 
 
 public class Partie {
@@ -25,120 +26,143 @@ public class Partie {
 
 		//on va probablement devoir utiliser un tableu de joueur
 
-		Joueur j=new Joueur("			Pierre",1);
-		score.nomJoueur(j.getNom());
+		/*
+		 * Joueur j=new Joueur("			Pierre",1); score.nomJoueur(j.getNom());
+		 * 
+		 * Joueur j2=new Joueur("Célia",2); score.nomJoueur(j2.getNom());
+		 * 
+		 * Joueur j3=new Joueur("Marine",3); score.nomJoueur(j3.getNom());
+		 */
 
-		Joueur j2=new Joueur("Célia",2);
-		score.nomJoueur(j2.getNom());
 
-		Joueur j3=new Joueur("Marine",3);
-		score.nomJoueur(j3.getNom());
+		Joueur [] tabJoueur = {new Joueur("pp",1),new Joueur("pp2",2),new Joueur("pp3",3)}; 
+		for(int i=0; i<tabJoueur.length; i++) {
+			score.nomJoueur(tabJoueur[i].getNom());
+		}
 
-		score.afficherScore();
 
 		System.out.println(tC.toString());
 
-		for(int numJoueur=1; numJoueur <= 3000; numJoueur++) {
-			boolean premierLance=true;
+		for(int numJoueur=0; numJoueur < 14; numJoueur++) {
+
+			for(int i=0; i<tabJoueur.length; i++) {
+				boolean premierLance=true;
+				boolean deuxiemeLance=true;
 
 
 
-			//total des dés
-			int somme=tC.totalDes(tC.des);
+				//total des dés
+				int somme=tC.totalDes(tC.des);
 
-			//on paramètre les combinaisons possibles
-			combi.setUn(tC.des);
-			combi.setDeux(tC.des);
-			combi.setTrois(tC.des);
-			combi.setQuatre(tC.des);
-			combi.setCinq(tC.des);
-			combi.setSix(tC.des);
+				//on paramètre les combinaisons possibles
+				combi.setUn(tC.des);
+				combi.setDeux(tC.des);
+				combi.setTrois(tC.des);
+				combi.setQuatre(tC.des);
+				combi.setCinq(tC.des);
+				combi.setSix(tC.des);
 
-			combi.setBrelan(tC.des);
-			combi.setCarre(tC.des);
-			combi.setFull(tC.des);
-			combi.setPsuite(tC.des);
-			combi.setGsuite(tC.des);
-			combi.setYam(tC.des);
+				combi.setBrelan(tC.des);
+				combi.setCarre(tC.des);
+				combi.setFull(tC.des);
+				combi.setPsuite(tC.des);
+				combi.setGsuite(tC.des);
+				combi.setYam(tC.des);
 
-			//on écrit ici le choix de la stratégie, exemple
-
-
-			if (combi.un!=0 && score.casDispo(1, j.getPosition())) {
-				score.barrerCase(1, j.getPosition(),combi.un);
-			}
-			else if (combi.deux!=0 && score.casDispo(2, j.getPosition())) {
-				score.barrerCase(2, j.getPosition(),combi.deux);
-			}
-			else if (combi.trois!=0 && score.casDispo(3, j.getPosition())) {
-				score.barrerCase(3, j.getPosition(),combi.trois);
-			}
-			else if (combi.quatre!=0 && score.casDispo(4, j.getPosition())) {
-				score.barrerCase(4, j.getPosition(),combi.quatre);
-			}
-			else if (combi.cinq!=0 && score.casDispo(5, j.getPosition())) {
-				score.barrerCase(5, j.getPosition(),combi.cinq);
-			}
-			else if (combi.six!=0 && score.casDispo(6, j.getPosition())) {
-				score.barrerCase(6, j.getPosition(),combi.six);
-			}
-			if (combi.aBrelan && score.casDispo(9, j.getPosition())) {
-				score.barrerCase(9, j.getPosition(),somme);
-			}
-			else if (combi.aCarre && score.casDispo(10, j.getPosition())) {
-				score.barrerCase(10, j.getPosition(),somme);
-			}
-			else if (combi.aFull && score.casDispo(11, j.getPosition())) {
-				score.barrerCase(11, j.getPosition(),25);
-			}
-			else if (combi.aPetiteSuite && score.casDispo(12, j.getPosition())) {
-				score.barrerCase(12, j.getPosition(),30);
-			}
-			else if (combi.aGrandeSuite && score.casDispo(13, j.getPosition())) {
-				score.barrerCase(13, j.getPosition(),40);
-			}
-			else if (combi.aYam && score.casDispo(14, j.getPosition())) {
-				score.barrerCase(14, j.getPosition(),50);
-			}
-			else if (score.casDispo(15, j.getPosition())) {
-				score.barrerCase(15, j.getPosition(),somme);
-			}
+				//on écrit ici le choix de la stratégie, exemple
 
 
-			//en dernier recours il veut relancer
-			/*
-			 * else { //tC.relancer( {1,1,1,1,1} ); //il choisit de relancer x dés, 5 ici
-			 * numJoueur=numJoueur-1; //puisqu'il relance on décrémente i
-			 * premierLance=false; //ça devient le 2ème lancé score.barrerCase(1,
-			 * j.getPosition(), 0); }
-			 */
-			
-			
-			//ajouter un événement "clic" pour faire le remplissage étape par étape
-			
-			int []tab= {0,1,2,3,4};
-			tC.relancer(tab);
+				if (combi.un!=0 && score.casDispo(1, tabJoueur[i].getPosition())) {
+					score.barrerCase(1, tabJoueur[i].getPosition(),combi.un);
+				}
+				else if (combi.deux!=0 && score.casDispo(2, tabJoueur[i].getPosition())) {
+					score.barrerCase(2, tabJoueur[i].getPosition(),combi.deux);
+				}
+				else if (combi.trois!=0 && score.casDispo(3, tabJoueur[i].getPosition())) {
+					score.barrerCase(3, tabJoueur[i].getPosition(),combi.trois);
+				}
+				else if (combi.quatre!=0 && score.casDispo(4, tabJoueur[i].getPosition())) {
+					score.barrerCase(4, tabJoueur[i].getPosition(),combi.quatre);
+				}
+				else if (combi.cinq!=0 && score.casDispo(5, tabJoueur[i].getPosition())) {
+					score.barrerCase(5, tabJoueur[i].getPosition(),combi.cinq);
+				}
+				else if (combi.six!=0 && score.casDispo(6, tabJoueur[i].getPosition())) {
+					score.barrerCase(6, tabJoueur[i].getPosition(),combi.six);
+				}
+				else if (combi.aBrelan && score.casDispo(9, tabJoueur[i].getPosition())) {
+					score.barrerCase(9, tabJoueur[i].getPosition(),somme);
+				}
+				else if (combi.aCarre && score.casDispo(10, tabJoueur[i].getPosition())) {
+					score.barrerCase(10, tabJoueur[i].getPosition(),somme);
+				}
+				else if (combi.aFull && score.casDispo(11, tabJoueur[i].getPosition())) {
+					score.barrerCase(11, tabJoueur[i].getPosition(),25);
+				}
+				else if (combi.aPetiteSuite && score.casDispo(12, tabJoueur[i].getPosition())) {
+					score.barrerCase(12, tabJoueur[i].getPosition(),30);
+				}
+				else if (combi.aGrandeSuite && score.casDispo(13, tabJoueur[i].getPosition())) {
+					score.barrerCase(13, tabJoueur[i].getPosition(),40);
+				}
+				else if (combi.aYam && score.casDispo(14, tabJoueur[i].getPosition())) {
+					score.barrerCase(14, tabJoueur[i].getPosition(),50);
+				}
+				else if (score.casDispo(15, tabJoueur[i].getPosition())) {
+					score.barrerCase(15, tabJoueur[i].getPosition(),somme);
+				}
+
+
+
+				//relance
+
+
+				/*
+				 * else if (deuxiemeLance) { numJoueur=numJoueur-1; //puisqu'il relance on
+				 * décrémente i i=i-1; if(!premierLance) { deuxiemeLance=false;
+				 * premierLance=true; } else premierLance=false;
+				 * 
+				 * 
+				 * //choix d'heuristique pour le choix des dés int []tab= {0,1,2,3,4};
+				 * tC.relancer(tab); }
+				 */
+
+				//ajouter un événement "clic" pour faire le remplissage étape par étape
+
+				//if(!deuxiemeLance) {
+				//int []tab= {0,1,2,3,4};
+				//tC.relancer(tab);
+
+				//}
+
+
+				//barrer une case si aucun cas choisi
+				else {
+					for(int j=1;j<17;j++) {
+						if(score.casDispo(j, tabJoueur[i].getPosition()) ) {
+							score.barrerCase(j, tabJoueur[i].getPosition(),0);
+							break;
+						}
+					}
+				}
+				
+				if(score.calculTotalSup(tabJoueur[i].getPosition())>62) {
+					score.grille[7][tabJoueur[i].getPosition()]="35";
+				}
+
+				int []tab= {0,1,2,3,4};
+				tC.relancer(tab);
+
+				score.calculTotalInf(i+1);
+				score.calculTotalSup(i+1);
+				score.calculTotal(i+1);
+			}
+
+
 		}
-		
-		
-		score.calculTotalInf(1);
-		score.calculTotalSup(1);
-		score.calculTotal(1);
+
 		score.afficherScore();
-
-
 	}
-
-
-	//relancer
-	//choix dans la grille (pas si il a déjà choisi avant, obligé sinon)
-	//il faut barrer dans le tableau une case
-	//calcul score actuel total du joueur
-
-	//Joueur suivant
-
-	//si plusieurs combinaisons sont vrais il faut implémenter une stratégie
-
 
 }
 
